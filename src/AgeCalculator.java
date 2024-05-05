2b59f2437f3d7e3f3fe49381c3130865c6e0a7d7
Gabriel Pires <102393858+gabrielboostphoenix@users.noreply.github.com>
2024-05-05 17:10:51 -0300

    Updating the file of age calculator where I've added some conditional statements to check if the user born year is valid or your respective age is valid for example.

--- src/AgeCalculator.java
+++ src/AgeCalculator.java
@@ -11,14 +11,38 @@ public class AgeCalculator {
         // Getting the current year from the system
         int currentYear = 2024;
         // Showing the message to the user to type your born year
-        System.out.print("Informe o seu ano de nascimento: ");
+        System.out.print("Informe o ano de nascimento do usuário: ");
         // Getting the born year of the user
         int bornYear = input.nextInt();
 
-        // Calculating the user age
-        int age = currentYear - bornYear;
-        // Showing a message to the user with your age information
-        System.out.printf("Com base nessa informação, sua idade é de %d anos", age);
+        // Checking if the born year is invalid
+        if (bornYear > currentYear) {
+            System.out.print("O ano de nascimento do usuário é inválido!");
+        } else {
+
+            // Calculating the user age
+            int age = currentYear - bornYear;
+            // Showing a message to the user with your age information
+            System.out.printf("Com base nessa informação, a idade deste usuário é de %d anos%n", age);
+
+            // Checking the user age to show a specific message about your age category
+            if (age >= 0 && age <= 2) {
+                System.out.print("Portanto o usuário é um bebê!");
+            } else if (age <= 9) {
+                System.out.print("Portanto o usuário é uma criança!");
+            } else if (age <= 12) {
+                System.out.print("Portanto o usuário é um pré-adolescente!");
+            } else if (age <= 17) {
+                System.out.print("Portanto o usuário é um adolescente");
+            } else if (age <= 64) {
+                System.out.print("Portanto o usuário é um adulto!");
+            } else if (age <= 100) {
+                System.out.print("Portanto o usuário é um idoso!");
+            } else {
+                System.out.print("Usuário com idade inválida!");
+            }
+
+        }
 
     }
 }
